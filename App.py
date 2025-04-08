750dadeaaa30d236f284196acb7150fd2810e4b6
Kaléu <kaleu.depaula9@gmail.com>
2025-04-07 23:50:10 -0300

    commit

--- App.py
+++ App.py
@@ -1,67 +1,29 @@
-from Utils.pastas            import Pastas
-from Utils.arquivos          import Arquivos
-from Manipulacao.organizador import Organizador
-from Manipulacao.mover       import Mover
-from Manipulacao.renomear    import Renomear
+from Interface.interface import Interface
 
-def exibir_menu():
-    print("\n--- Menu de Opções ---")
-    print("1. Organizar arquivos por extensão")
-    print("2. Mover arquivo")
-    print("3. Renomear arquivo")
-    print("4. Sair")
+class Main:
+    def __init__(self):
+        self.interface = Interface()
 
-    opcao = input("Escolha uma opção: ")
-    return opcao
+    def start(self):
+        opcoes = {
+            '1': Interface.organizar_arquivos,
+            '2': self.interface.mover_arquivo,
+            '3': self.interface.renomear_arquivo,
+            '4': lambda: print("Saindo do programa...") or exit()
+        }
 
-def organizar_arquivos():
-    diretorio   = input("Digite o diretório onde os arquivos estão localizados: ")
-    organizador = Organizador()
-    organizador.organizar_por_extensao(diretorio)
+        try:
+            while True:
+                opcao = self.interface.exibir_menu()
 
-def mover_arquivo():
-    origem  = input("Digite o caminho do arquivo de origem: ")
-    destino = input("Digite o caminho da pasta de destino: ")
-    
-    mover = Mover()
-    mover.mover_arquivo(origem, destino)
-
-def renomear_arquivo():
-    caminho   = input("Digite o caminho do arquivo a ser renomeado: ")
-    novo_nome = input("Digite o novo nome do arquivo (sem a extensão): ")
-    
-    renomear = Renomear()
-    renomear.renomear_arquivo(caminho, novo_nome)
-
-def listar_arquivos():
-    diretorio = input("Digite o diretório para listar arquivos: ")
-    arquivos  = Arquivos()
-    lista     = arquivos.listar_arquivos(diretorio)
-    
-    print("Arquivos encontrados:")
-    for arquivo in lista:
-        print(arquivo)
-
-def criar_pasta():
-    diretorio = input("Digite o diretório da pasta a ser criada: ")
-    pastas = Pastas()
-    pastas.criar_pasta(diretorio)
-
-def main():
-    opcoes = {
-        '1': organizar_arquivos,
-        '2': mover_arquivo,
-        '3': renomear_arquivo,
-        '4': lambda: print("Saindo do programa...") or exit()
-    }
-
-    while True:
-        opcao = exibir_menu()
-
-        if opcao in opcoes:
-            opcoes[opcao]()  
-        else:
-            print("Opção inválida. Tente novamente.")
+                if opcao in opcoes:
+                    opcoes[opcao]()  
+                else:
+                    print("Opção inválida! Tente novamente.")
+        except KeyboardInterrupt:
+            print("\nAplicação Finalizada...")
+            exit()
 
 if __name__ == "__main__":
-    main()
+    main = Main()
+    main.start()
---
+++ Interface/interface.py
@@ -0,0 +1,52 @@
+from Manipulacao.organizador import Organizador
+from Manipulacao.mover import Mover
+from Manipulacao.renomear import Renomear
+from Utils.arquivos import Arquivos
+from Utils.pastas import Pastas
+
+class Interface:
+    def __init__(self):
+        pass
+
+    def exibir_menu(self):
+        print("\n--- Menu de Opções ---\n")
+        print("1. Organizar arquivos por extensão")
+        print("2. Mover arquivo")
+        print("3. Renomear arquivo")
+        print("4. Sair")
+
+        opcao = input("\nEscolha uma opção: ")
+        return opcao
+
+    def organizar_arquivos(self):
+        diretorio = input("Digite o diretório onde os arquivos estão localizados: ")
+        organizador = Organizador()
+        organizador.organizar_por_extensao(diretorio)
+
+    def mover_arquivo(self):
+        origem  = input("Digite o caminho do arquivo de origem: ")
+        destino = input("Digite o caminho da pasta de destino: ")
+        
+        mover = Mover()
+        mover.mover_arquivo(origem, destino)
+
+    def renomear_arquivo(self):
+        caminho   = input("Digite o caminho do arquivo a ser renomeado: ")
+        novo_nome = input("Digite o novo nome do arquivo (sem a extensão): ")
+        
+        renomear = Renomear()
+        renomear.renomear_arquivo(caminho, novo_nome)
+
+    def listar_arquivos(self):
+        diretorio = input("Digite o diretório para listar arquivos: ")
+        arquivos  = Arquivos()
+        lista     = arquivos.listar_arquivos(diretorio)
+        
+        print("Arquivos encontrados:")
+        for arquivo in lista:
+            print(arquivo)
+
+    def criar_pasta(self):
+        diretorio = input("Digite o diretório da pasta a ser criada: ")
+        pastas = Pastas()
+        pastas.criar_pasta(diretorio)
